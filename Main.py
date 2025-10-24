a9127f848c19ad88e9a365e1da05bfd2979ec32e
Ernest Wheaton <e3rules@gmail.com>
2025-10-23 20:17:34 -0400

    Implement ViT

--- Main.py
+++ Main.py
@@ -1,45 +1,30 @@
-from torch.utils.data import DataLoader, Subset
-from torchvision.datasets import CIFAR10
-from torchvision import transforms
-from torch import randperm, no_grad, device, cuda
+from torch import no_grad, device, cuda
 from models.dnn import ResNet18For10class
+from models.vit import ViTFor10Class
 from attack.attack_types import UntargetedAttack
+from dataloader.cifarloader import CifarDataLoader
 dev = device("cuda" if cuda.is_available() else "cpu")
 
 if __name__ == "__main__":
     #entry point of program
-    model = ResNet18For10class("cifar10test")
-
-    # move this to some data loader abstraction
-    # DO NOT continue to pollute this file with implementation
-    rgbT = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize(224),
-            # cifar10 normalization values (known)
-            transforms.Normalize((.5,.5,.5), (.5,.5,.5))
-        ])
-    # Much of this code is adapted from HW1
-    # There is a lot here that can be dropped.
-    # since this is an initial test script, cleanliness is not needed
-    # but it will be needed at some point
-    cifar10data = CIFAR10(root='./data', train=False, download=True, transform=rgbT)
-    testDataLoader = DataLoader(cifar10data, batch_size=64, shuffle=True)
-    cifartraindata = CIFAR10("./data", train=True, download=True, transform=rgbT)
-    indices = randperm(len(cifar10data))[:64]
-    advData = Subset(cifar10data, indices)
-    advDataLoader = DataLoader(advData, batch_size=64, shuffle=True)
-    model = model.to(device=dev)
+    #model = ResNet18For10class("cifar10test")
+    model = ViTFor10Class("cifarvit10test")
+    data = CifarDataLoader()
     tries = 0
     success = 0
     
     if model.isPretrainOnDisk():
         model.load()
+        model = model.to(device=dev)
     else:
-        model.training_loop(cifartraindata, None)
+        model = model.to(device=dev)
+        training = data.get_train_data()
+        model.training_loop(training)
         model.save()
 
+    test = data.get_test_data_iterable()
     with no_grad():
-        for image, label in testDataLoader:
+        for image, label in test:
             image = image.to(device=dev)
             label = label.to(device=dev)
             _, pred = model(image).max(1)
@@ -48,5 +33,6 @@ if __name__ == "__main__":
     acc = 100 * (success/tries)
     print(f"clean accuracy={acc}")
 
-    attack = UntargetedAttack(model, model.getLoss(), advDataLoader, lr=.001)
+    adv = data.get_random_test_slice()
+    attack = UntargetedAttack(model, model.getLoss(), adv, lr=.001)
     attack.execute_attack()
